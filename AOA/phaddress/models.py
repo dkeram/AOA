b6fa5055b52526823884d1b8e2fd8e3f9ac8c673
Dimitris <jim600v@gmail.com>
2022-11-10 01:22:36 +0200

    feat: Create model for phaddress app

--- AOA/phaddress/models.py
+++ AOA/phaddress/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Phones(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number_1 = models.IntegerField(max_length=9)
+    phone_number_2 = models.IntegerField(max_length=9)
+    email = models.EmailField()
